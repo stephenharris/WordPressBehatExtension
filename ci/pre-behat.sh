a9d6cbb3b1d9be622ae76835780b0f286b0676ab
Stephen Harris <contact@stephenharris.info>
2016-10-10 16:19:26 +0100

    Download Selenium

--- ci/pre-behat.sh
+++ ci/pre-behat.sh
@@ -21,6 +21,7 @@ echo 'start php';
 php -S localhost:8000 -t $WORDPRESS_SITE_DIR -d disable_functions=mail > /dev/null 2>&1 &
 
 # Start Selenium
+wget http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.1.jar
 java -jar selenium-server-standalone-2.53.1.jar -p $SELENIUM_PORT > /dev/null 2>&1 &
 
 # Wait for Selenium, if necessary
