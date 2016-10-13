fbeda7551d3874ac38b36d57b8f0259dd804f7f0
Stephen Harris <contact@stephenharris.info>
2016-10-13 14:10:42 +0100

    Fix casing and formatting

--- src/StripHtml.php
+++ src/StripHtml.php
@@ -1,36 +1,34 @@
 <?php
 namespace StephenHarris\WordPressBehatExtension;
 
-trait stripHtml
+trait StripHtml
 {
 
-    public function stripTagsAndContent( $html ) {
+    public function stripTagsAndContent($html)
+    {
 
-        if ( trim($html) == '' ) {
+        if (trim($html) == '') {
             return $html;
         }
 
         $doc = new \DOMDocument();
         $doc->loadHTML("<div>{$html}</div>");
 
-        $container = $doc->getElementsByTagName( 'div' )->item( 0 );
+        $container = $doc->getElementsByTagName('div')->item(0);
 
         //Remove nodes while iterating over them does not work
         //@link http://php.net/manual/en/domnode.removechild.php#90292
         $removeQueue = array();
-        foreach( $container->childNodes as $childNode )
-        {
-            if ( $childNode->nodeType !== XML_TEXT_NODE ) {
+        foreach ($container->childNodes as $childNode) {
+            if ($childNode->nodeType !== XML_TEXT_NODE) {
                 $removeQueue[] = $childNode;
             }
         }
 
-        foreach( $removeQueue as $node )
-        {
+        foreach ($removeQueue as $node) {
             $container->removeChild($node);
         }
 
-        return trim( $container->textContent );
+        return trim($container->textContent);
     }
-
 }
