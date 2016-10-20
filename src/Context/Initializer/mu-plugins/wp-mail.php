0c3180be429f3cfe82c674bf291fdd50e5d2d222
Stephen Harris <contact@stephenharris.info>
2016-10-20 11:17:14 +0100

    Add support for headers & attachments

--- src/Context/Initializer/mu-plugins/wp-mail.php
+++ src/Context/Initializer/mu-plugins/wp-mail.php
@@ -10,18 +10,21 @@
  *
  * @return bool True if the email got sent (i.e. if the fake email file was written)
  */
-function wp_mail($recipients, $subject, $message)
+function wp_mail($recipients, $subject, $message, $headers = '', $attachments = array())
 {
-    $recipients = is_array($recipients) ? $recipients : array( $recipients );
+    $recipients = is_array($recipients) ? $recipients : explode(',', $recipients);
+    $recipients = array_map('trim', $recipients);
 
     foreach ($recipients as $recipient) {
         $file_name = sanitize_file_name(time() . "-$recipient-" . sanitize_title_with_dashes($subject));
         $file_path = trailingslashit(WORDPRESS_FAKE_MAIL_DIR) . $file_name;
 
         $data = array(
-            'to'      => $recipient,
-            'subject' => $subject,
-            'message' => $message
+            'to'          => $recipient,
+            'subject'     => $subject,
+            'message'     => $message,
+            'headers'     => $headers,
+            'attachments' => $attachments,
         );
 
         $result = (bool) file_put_contents($file_path, json_encode($data));
