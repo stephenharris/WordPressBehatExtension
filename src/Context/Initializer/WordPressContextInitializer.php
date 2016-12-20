ef4a9d07e3a0ea2c758c5a81911443eef8b76f43
Stephen Harris <contact@stephenharris.info>
2016-12-20 16:40:34 +0000

    Fix populting of HTTP_HOST

--- src/Context/Initializer/WordPressContextInitializer.php
+++ src/Context/Initializer/WordPressContextInitializer.php
@@ -53,7 +53,8 @@ class WordPressContextInitializer implements ContextInitializer
      */
     private function prepareEnvironment()
     {
-        $_SERVER['HTTP_HOST'] = $this->wordpressParams['site_url'];
+        $urlParts = parse_url($this->wordpressParams['site_url']);
+        $_SERVER['HTTP_HOST']       = $urlParts['host'] . (isset($urlParts['port']) ? ':' . $urlParts['port'] : '');
 
         if ($this->wordpressParams['mail']['directory'] && !is_dir($this->wordpressParams['mail']['directory'])) {
             mkdir($this->wordpressParams['mail']['directory'], 0777, true);
